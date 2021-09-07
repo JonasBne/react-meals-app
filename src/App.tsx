1859ec7eac7a8bd20330b31e580c374db6de22bf
Jonas Boone <jonas.boone@student.kdg.be>
2021-09-07 11:33:36 +0200

    Added a meals component

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
 import React from "react";
 
 import Header from "./components/Layout/Header";
-import MealsSummary from "./components/Meals/MealsSummary";
-import AvailableMeals from "./components/Meals/AvailableMeals";
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
       <React.Fragment>
           <Header />
-          <MealsSummary />
-          <AvailableMeals meals={DUMMY_MEALS} />
+          <main>
+              <Meals />
+          </main>
       </React.Fragment>
   );
 }
--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,28 +1,54 @@
 import React from "react";
 
-type Meal = {
+import classes from "./AvailableMeals.module.css"
+
+interface Meal {
     id: string,
     name: string,
     description: string,
     price: number
 }
 
-interface IProps {
-    meals: Array<Meal>
-}
 
 
-const AvailableMeals =  (props: IProps) => {
+const DUMMY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
+];
+
+const AvailableMeals =  () => {
+    const mealsList = DUMMY_MEALS.map((item: Meal) => {
+        return <li key={item.id}>{item.name}</li>
+    })
+
     return (
-        <React.Fragment>
-            <ul>
-                {
-                    props.meals.map((item: Meal) => {
-                        return <li key={item.id}>{item.name}</li>
-                    })
-                }
+        <section>
+            <ul className={classes.mealsList}>
+                {mealsList}
             </ul>
-        </React.Fragment>
+        </section>
     )
 }
 
