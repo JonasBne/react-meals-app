425d2f41c8b895185b116ed1dc92c0f880ae2c1b
Jonas Boone <jonas.boone@student.kdg.be>
2021-09-09 19:10:49 +0200

    Added first component of a login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import HeaderLogin from "./components/Layout/HeaderLogin";
 
 function App() {
 
@@ -16,8 +17,13 @@ function App() {
       setShowCart(false);
   }
 
-
   return (
+      <React.Fragment>
+          <HeaderLogin />
+      </React.Fragment>
+  )
+
+/*  return (
       <React.Fragment>
           <Header onShowCart={showCartHandler} />
           {showCart ? <Cart onHideCart={hideCartHandler} /> : null}
@@ -25,7 +31,7 @@ function App() {
               <Meals />
           </main>
       </React.Fragment>
-  );
+  );*/
 }
 
 export default App;
---
+++ src/components/Layout/HeaderLogin.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import classes from "./HeaderLogin.module.css";
+
+const HeaderLogin = () => {
+
+    return (
+        <React.Fragment>
+            <header className={classes["header-login"]}>
+                <h1>ReactMeals</h1>
+                <div>
+                    <button className={classes.button}>Log in</button>
+                    <button className={classes.button}>Sign up</button>
+                </div>
+            </header>
+        </React.Fragment>
+    )
+}
+
+export default HeaderLogin;
